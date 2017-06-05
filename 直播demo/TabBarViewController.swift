a84561c072cda5293166c769fb68d858b93b96a7
周少停 <zst@9elelphas.com>
2017-06-05 11:52:46 +0800

    增加第三个,第四个tab
    增加第三个,第四个tab

--- 直播demo/TabBarViewController.swift
+++ 直播demo/TabBarViewController.swift
@@ -11,14 +11,19 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-         addChildVCandRootVC(navTitle: "直播列表", tabTitle: "直播", rootVC: OneViewController())
-         addChildVCandRootVC(navTitle: "歌曲列表", tabTitle: "歌曲", rootVC: TwoViewController())
+         addChildVCandRootVC(navTitle: "直播列表", tabTitle: "直播", rootVC: OneViewController(),img: #imageLiteral(resourceName: "1_"),img_selected: #imageLiteral(resourceName: "1_selected"))
+         addChildVCandRootVC(navTitle: "歌曲列表", tabTitle: "歌曲", rootVC: TwoViewController(),img: #imageLiteral(resourceName: "2_"),img_selected: #imageLiteral(resourceName: "2_selected"))
+         addChildVCandRootVC(navTitle: "3", tabTitle: "三", rootVC: ThreeViewController(),img: #imageLiteral(resourceName: "3_"),img_selected: #imageLiteral(resourceName: "3_selected"))
+         addChildVCandRootVC(navTitle: "4", tabTitle: "四", rootVC: FourViewController(),img: #imageLiteral(resourceName: "4_"),img_selected: #imageLiteral(resourceName: "4_selected"))
+        
     }
     
-    func addChildVCandRootVC(navTitle:String,tabTitle:String,rootVC:UIViewController){
+    func addChildVCandRootVC(navTitle:String,tabTitle:String,rootVC:UIViewController,img:UIImage,img_selected:UIImage){
         let rNav = UINavigationController.init(rootViewController: rootVC)
         rootVC.title = navTitle
-        rNav.title =  tabTitle
+        rootVC.tabBarItem.image = img
+        rootVC.tabBarItem.selectedImage = img_selected
+        rootVC.tabBarItem.title = tabTitle
         self.addChildViewController(rNav)
     }
 

6111154389e75d0425c8292d5c772b624cf45e92
周少停 <zst@9elelphas.com>
2017-06-07 14:26:25 +0800

    增加第四个tab的按钮扩展和时间轴效果
    增加第四个tab的按钮扩展和时间轴效果

--- 直播demo/第四个tab/FourViewController.swift
+++ 直播demo/第四个tab/FourViewController.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 class FourViewController: UIViewController {
-
+  let SCREENW =  UIScreen.main.bounds.size.width
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let btn1  = UIButton.init(frame: CGRect.init(x: 0, y: 74, width: SCREENW * 0.5 - 5, height: 50))
+        btn1.setTitle("按钮扩展", for: .normal)
+        btn1.setTitleColor(UIColor.red, for: .normal)
+        btn1.addTarget(self, action: #selector(FourViewController.btnExtensionAction), for: .touchUpInside)
+        self.view.addSubview(btn1)
+        
+        let btn2  = UIButton.init(frame: CGRect.init(x: SCREENW * 0.5, y: 74, width: SCREENW * 0.5 - 5, height: 50))
+        btn2.setTitle("时间轴", for: .normal)
+        btn2.setTitleColor(UIColor.red, for: .normal)
+        btn2.addTarget(self, action: #selector(FourViewController.timeLineAction), for: .touchUpInside)
+        self.view.addSubview(btn2)
+        
     }
 
+    func btnExtensionAction(){
+        self.navigationController?.pushViewController(BtnViewController(), animated: false)
+    }
+    
+    func timeLineAction(){
+         self.navigationController?.pushViewController(TimeLineViewController(), animated: false)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ 直播demo/第四个tab/按钮扩展/BtnViewController.swift
@@ -0,0 +1,48 @@
+//
+//  BtnViewController.swift
+//  直播demo
+//
+//  Created by Shaoting Zhou on 2017/6/7.
+//  Copyright © 2017年 Shaoting Zhou. All rights reserved.
+//
+
+import UIKit
+
+class BtnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let btn1 = UIButton.init(frame: CGRect.init(x: 50, y: 100, width: 200, height: 100))
+        self.view.addSubview(btn1)
+        let btn2 = UIButton.init(frame: CGRect.init(x: 50, y: 200, width: 200, height: 100))
+        self.view.addSubview(btn2)
+        let btn3 = UIButton.init(frame: CGRect.init(x: 50, y: 300, width: 200, height: 100))
+        self.view.addSubview(btn3)
+        let btn4 = UIButton.init(frame: CGRect.init(x: 50, y: 400, width: 200, height: 100))
+        self.view.addSubview(btn4)
+        
+        btn1.setImageAndTitle(imageName: "gift", title: "我是按钮", type: .PositionTop, Space: 0.0)
+        btn2.setImageAndTitle(imageName: "gift", title: "我是按钮", type: .PositionBottom, Space: 0.0)
+        btn3.setImageAndTitle(imageName: "gift", title: "我是按钮", type: .Positionleft, Space: 0.0)
+        btn4.setImageAndTitle(imageName: "gift", title: "我是按钮", type: .PositionRight, Space: 0.0)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

edc1f7dad14fe964724bfd9c6fef95a384f5d2bf
周少停 <zst@9elelphas.com>
2017-06-09 13:34:44 +0800

    转回原生一周,简单学习下
    转回原生一周,简单学习下

--- 直播demo/第三个tab/分页控制器/PageViewController.swift
+++ 直播demo/第三个tab/分页控制器/PageViewController.swift
@@ -37,7 +37,13 @@ class PageViewController: UIViewController {
         view.addSubview(pageView)
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = false
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
